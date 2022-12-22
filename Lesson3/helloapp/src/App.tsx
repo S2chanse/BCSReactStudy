dd3b27f31de26f3569b277d4bea7a3c2e476e835
css <seop7379@daum.net>
2022-12-22 15:53:56 +0900

    vite typescript react start

---
+++ Lesson3/helloapp/src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+type Props = {};
+
+function App(props: Props) {
+  return (
+    <div>
+      <h3>Hello World!</h3>
+    </div>
+  );
+}
+
+export default App;
