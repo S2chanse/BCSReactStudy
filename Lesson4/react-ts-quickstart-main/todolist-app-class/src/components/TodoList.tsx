1ce461b96eabd88dbc9cec97103eac88e126047f
css <seop7379@daum.net>
2022-12-26 17:07:08 +0900

    shouldComponentUpdate test folder make

---
+++ Lesson4/react-ts-quickstart-main/todolist-app-class/src/components/TodoList.tsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { TodoListItemType } from '../AppContainer';
+import TodoListItem from './TodoListItem';
+
+type Props = {
+  todoList: Array<TodoListItemType>;
+  toggleDone: Function;
+  deleteTodo: Function;
+};
+
+export default class TodoList extends Component<Props> {
+  render() {
+    console.log('## TodoList 렌더');
+    let items = this.props.todoList.map((item: TodoListItemType) => {
+      return (
+        <TodoListItem
+          key={item.no}
+          todoItem={item}
+          deleteTodo={this.props.deleteTodo}
+          toggleDone={this.props.toggleDone}
+        />
+      );
+    });
+
+    return (
+      <div className='row'>
+        {' '}
+        <div className='col'>
+          <ul className='list-group'>{items}</ul>
+        </div>
+      </div>
+    );
+  }
+}
