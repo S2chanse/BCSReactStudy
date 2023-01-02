c6e3d561fe7831b81fd4fe5c38ebf251575180de
css <seop7379@daum.net>
2023-01-02 12:19:36 +0900

    mouse 이동 함수 페이지 추가

--- Lesson6/hof-test-app/src/connectMousePos.tsx
+++ Lesson6/hof-test-app/src/connectMousePos.tsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-type Props = {};
+export type PositionType = {
+  x: number;
+  y: number;
+};
 
-const connectMousePos = (props: Props) => {
-  return <div>connectMousePos</div>;
+type Props = {
+  TargetComponent: React.ComponentType<any>;
+};
+
+const connectMousePos = ({ TargetComponent }: Props) => {
+  return (props: any) => {
+    const [position, setPosition] = useState<PositionType>({ x: 0, y: 0 });
+    useEffect(() => {
+      const onMove = (e: MouseEvent) => setPosition({ x: e.pageX, y: e.pageY });
+      window.addEventListener('mousemove', onMove);
+
+      return () => {
+        window.removeEventListener('mousemove', onMove);
+      };
+    }, []);
+
+    return <TargetComponent {...props} position={position} />;
+  };
 };
